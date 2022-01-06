a06aa8546a58e9aa5b626b8cdf36f761ea63164b
Your Name <you@example.com>
2022-01-06 23:59:10 +0300

    add admin frontend/backend

--- docker/loader/scripts/init.sh
+++ docker/loader/scripts/init.sh
@@ -4,5 +4,6 @@ mkdir -p /etc/cloud_validol
 cat /app/scripts/secdist.json.tmpl | envsubst > /etc/cloud_validol/secdist.json
 
 service cron start
+supervisord -c /root/supervisor.conf
 
 tail -F /var/log/validol_scraper.log
